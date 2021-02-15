1ff5e8d803cce905653d08f34bda67bc61944857
run-after <run.after@protonmail.com>
2021-02-15 16:17:20 -0600

    Display Login / SignUp and pass in props to Header/Login/SignUp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './styles/App.css';
 import Header from './components/Header';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import { useState } from 'react';
 
 function App() {
+
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
     <div className="App">
-      <Header />
-      Oh, Hi!
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+      <Login setLoggedIn={setLoggedIn} />
+      <SignUp setLoggedIn={setLoggedIn} />
     </div>
   );
 }
