5a42b6f73875a180bc1291f288208d743e62075e
run-after <run.after@protonmail.com>
2021-02-15 16:15:27 -0600

    Make Header display correct buttons based on if user logged in or not

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,36 @@
 import '../styles/Header.css';
 import moon from '../media/moon.png';
 import logo from '../media/alien.png';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
-const Header = () => {
+const Header = (props) => {
+
+  const showLogin = () => {
+    const form = document.querySelector('.login-form');
+    form.style = 'display: flex;'
+  };
+
+  const showSignUp = () => {
+    const form = document.querySelector('.sign-up-form');
+    form.style = 'display: flex';
+  };
+
+  const logOut = () => {
+    firebase.auth().signOut().then(() => {
+      props.setLoggedIn(false);
+    });
+  };
   
   return (
     <div className='header'>
       <img className='logo' alt='logo' src={logo} />
       <div className='buttons'>
-        <button className='login-btn btn'>Login</button>
-        <button className='sign-up-btn btn'>Sign up</button>
+        {!props.loggedIn && <button onClick={showLogin} className='login-btn btn'>Login</button>}
+        {props.loggedIn && <button onClick={logOut}className='log-out-btn btn'>Log out</button>}
+        {!props.loggedIn && <button onClick={showSignUp} className='sign-up-btn btn'>Sign up</button>}
       </div>
-      <button className='night-mode'><img src={moon} /></button>
+      <button className='night-mode'><img alt='moon' src={moon} /></button>
       
     </div>
   );
