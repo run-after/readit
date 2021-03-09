13a4361dc06d72e846b904890bf570e61aab7768
run-after <run.after@protonmail.com>
2021-03-09 11:16:55 -0600

    Add route to groups index page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SignUp from './components/SignUp';
 import Feed from './components/Feed';
 import User from './components/User';
 import Group from './components/Group';
+import Groups from './components/Groups';
 import { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
@@ -29,7 +30,8 @@ function App() {
           <Route exact path='/login' render={() => <Login user={user} setUser={setUser} />} />
           <Route exact path='/signup' render={() => <SignUp user={user} setUser={setUser} />} />
           <Route exact path='/user/:name' render={() => <User />} />
-          <Route exact path='/groups/:group' render={() => <Group user={user} /> } />
+          <Route exact path='/groups/:group' render={() => <Group user={user} />} />
+          <Route exact path='/groups' render={() => <Groups user={user} />} />
         </Switch>
       </BrowserRouter>
     </div>
@@ -39,13 +41,11 @@ function App() {
 export default App;
 
 /*
-  First make groups and link those groups on each post.
-  List groups in right hand margin?
-  Need a way to make a new group
-
   Make unfound route go to 404
 
   Up/down vote arrows need to be able to turn color
 
   Button on right margin to make new pic post doesn't work
+
+  Maybe move group list from header to right margin to fill space???
 */
