f2e523ccbbde0300cc42a9773fb15b74d182796c
run-after <run.after@protonmail.com>
2021-03-14 16:55:33 -0500

    Move all DB calls to here. Then pass down needed info to components

--- src/App.js
+++ src/App.js
@@ -23,7 +23,10 @@ function App() {
 
   const [user, setUser] = useState(initialUser);
   const [userRef, setUserRef] = useState(null);
-  console.log(userRef)
+  const [allPosts, setAllPosts] = useState(null);
+  const [allGroups, setAllGroups] = useState(null);
+  const [allComments, setAllComments] = useState(null);
+
   useEffect(() => {
     if (user) {
       firebase.firestore().collection('users').doc(user.displayName).get().then((doc) => {
@@ -32,16 +35,46 @@ function App() {
     };
   }, [user]);
 
+  useEffect(() => {
+    // Get all posts
+    firebase.firestore().collection('posts').get().then((querySnapShot) => {
+      let tempPosts = {};
+      querySnapShot.forEach((x) => {
+        tempPosts[x.id] = x.data();
+      });
+      setAllPosts(tempPosts);
+    });
+
+    // Get all comments
+    firebase.firestore().collection('comments').get().then((querySnapShot) => {
+      let tempComments = {};
+      querySnapShot.forEach((x) => {
+        tempComments[x.id] = x.data();
+      });
+      setAllComments(tempComments);
+    });
+
+    // Get all groups (don't love the object structure)
+    firebase.firestore().collection('groups').get().then((querySnapShot) => {
+      let tempGroups = [];
+      querySnapShot.forEach((x) => {
+        tempGroups.push(x.id);
+      });
+      setAllGroups(tempGroups);
+    });
+
+  }, [])
+
   return (
     <div className="App">
       <BrowserRouter>
         <Header user={user} setUser={setUser} userRef={userRef}/>
         <Switch>
-          <Route exact path='/' render={() => <Feed user={user} userRef={userRef} />} />
+          <Route exact path='/' render={() => <Feed userRef={userRef} posts={allPosts} setAllPosts={setAllPosts} groups={allGroups} />} />
           <Route exact path='/login' render={() => <Login user={user} setUser={setUser} />} />
           <Route exact path='/signup' render={() => <SignUp user={user} setUser={setUser} setUserRef={setUserRef} />} />
           <Route exact path='/user/:name' render={() => <User user={user} userRef={userRef} />} />
-          <Route exact path='/groups/:group' render={() => <Group user={user} userRef={userRef}/>} />
+          <Route exact path='/groups/:group' render={() => <Group user={user} userRef={userRef} groups={allGroups} posts={allPosts}/>} />
           <Route exact path='/groups' render={() => <Groups user={user} setUserRef={setUserRef} />} />
         </Switch>
       </BrowserRouter>
@@ -52,16 +85,18 @@ function App() {
 export default App;
 
 /*
+  Work in Feed
 
-  Work to be done in Group.js
+  Don't love allGroups object structure
 
   Make unfound route go to 404
-
-  Button on right margin of Feed to make new pic post doesn't work
 
   Remove link from Header with name. When on a different user page, it doesn't update info
 
   Maybe move group list from header to right margin to fill space???
 
   Maybe, when first making a post or a comment, it's upvoted by you right away
+
+  Warning: Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function.
+  ^ To fix, try to move the post retrieving from feed to App. Then it only does it once.
 */
